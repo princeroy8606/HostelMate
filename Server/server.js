b44d668c659f1a9c6d10e432d7ddd3eec3742e4e
vyshakhmk <83387075+princeroy8606@users.noreply.github.com>
2023-03-09 19:16:51 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const mongoose= require("mongoose");
-
 const app = express();
-
-
 app.use(express.json({limit:"40mb", extended:true}))
 
 app.get("/",(req,res)=>{
