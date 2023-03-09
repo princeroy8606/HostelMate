54317eaa93ef8340bd8dc033ce335776f9b35b5e
vyshakhmk <83387075+princeroy8606@users.noreply.github.com>
2023-03-09 19:14:48 +0530

    updates

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,20 @@
-console.log("running suh")
+const express = require("express");
+const mongoose= require("mongoose");
+
+const app = express();
+
+
+app.use(express.json({limit:"40mb", extended:true}))
+
+app.get("/",(req,res)=>{
+    res.send("The hostel management app api");
+});
+
+const CONNECTION_URL="mongodb+srv://princeroy8606:hostelmanagement116@cluster0.xdbj8ps.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(CONNECTION_URL)
+.then(()=>{
+    app.listen(3000,()=>{
+        console.log("Hello world")
+     })
+})
