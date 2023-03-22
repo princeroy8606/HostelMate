09053aeb69bf58324767049e82b387ca7dfccade
vyshakhmk <83387075+princeroy8606@users.noreply.github.com>
2023-03-22 23:54:16 +0530

    Update App.js

--- Client/App.js
+++ Client/App.js
@@ -2,6 +2,7 @@ import { StatusBar, Text, View , Dimensions} from 'react-native';
 import AuthNav from './Navigation/AuthNav';
 import { Provider } from 'react-redux';
 
+
 export default function App() {
   return (
     <View style={{flex:1}}>
